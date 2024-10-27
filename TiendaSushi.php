ad6d23b850f999e77c3b0a662db58930fd4a391a
wotaegui <wotaegui@gmail.com>
2024-10-27 19:57:31 -0300

    Carga inicial

---
+++ TiendaSushi.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Fukusuke Sushi</title>
+  <!-- Bootstrap CSS via CDN -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <!-- Contenido de tu página aquí -->
+  <h1 class="text-center">Bienvenidos a Fukusuke Sushi</h1>
+  <!-- Bootstrap JS via CDN -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
